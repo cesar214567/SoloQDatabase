07a471d861d876ed93c636292be77650a560acf0
cesar214567 <camgcamg11@gmail.com>
2022-12-13 08:56:25 -0500

    changing credentials

--- db/schema.rb
+++ db/schema.rb
@@ -14,53 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_031800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "dish_ingredients", force: :cascade do |t|
-    t.bigint "dish_id", null: false
-    t.bigint "ingredient_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["dish_id"], name: "index_dish_ingredients_on_dish_id"
-    t.index ["ingredient_id"], name: "index_dish_ingredients_on_ingredient_id"
-  end
-
-  create_table "dishes", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.float "cost"
-    t.bigint "restaurant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
-  end
-
   create_table "elo_timestamps", force: :cascade do |t|
     t.integer "elo"
     t.bigint "summoner_id", null: false
@@ -87,39 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_031800) do
     t.index ["summoner_id"], name: "index_games_on_summoner_id"
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "players", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "loc"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_restaurants_on_user_id"
-  end
-
-  create_table "scores", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.integer "score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["player_id"], name: "index_scores_on_player_id"
-  end
-
   create_table "summoners", force: :cascade do |t|
     t.string "name"
     t.string "summoner_name"
@@ -128,30 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_031800) do
     t.string "gg_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-    t.string "country"
-    t.string "city"
-    t.string "address"
-    t.datetime "birth_date", precision: nil
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "dish_ingredients", "dishes"
-  add_foreign_key "dish_ingredients", "ingredients"
   add_foreign_key "elo_timestamps", "summoners"
   add_foreign_key "games", "summoners"
-  add_foreign_key "scores", "players"
 end
